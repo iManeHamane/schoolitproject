5889817bb51a0ca3ef90e6d8db5ce19f18fdedd9
iManeHamane <imanehamane5@gmail.com>
2022-05-29 11:22:26 +0100

    second commit

---
+++ src/pages/Parametres/VerifEmail.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import './VerifEmail.css';
+
+
+
+const VerifEmail = ()=> {
+    
+    return (
+        <>
+
+        <h3 className="bute" text-align="center" style={{paddingTop: "15%" }} > Veuillez vérifier votre email. Vous trouverez un email avec un lien de vérification dans votre boîte mail</h3>
+        <br></br> <br></br>
+        <input className="but" type="button" value="Se connecter" ></input>             
+        </>   
+    )
+}
+
+export default VerifEmail;
